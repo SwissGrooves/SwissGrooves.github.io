57049be19eb20e3a078102cc0ee88af01c480eb9
Alexander Fleischli <alex.fleischli@gmail.com>
2019-03-05 21:13:18 +0100

    Added great pun

--- js/script.js
+++ js/script.js
@@ -8,7 +8,8 @@ $(document).ready(function() {
 		"Hier dreht sich alles um Musik",
 		"Wir halten mehrere Records",
 		"Alles intakt",
-		"Im Angebot: Chai(kovsky)"
+		"Im Angebot: Chai(kovsky)",
+		"Vinyl-st du etwas neues hören?"
 	];
 
 	var randomTagline = taglines[Math.floor(Math.random()*taglines.length)];
