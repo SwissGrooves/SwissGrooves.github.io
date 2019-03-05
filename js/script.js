74411c28e4ff42447eee7fe4871204e0ba762a4e
Alexander Fleischli <alex.fleischli@gmail.com>
2019-03-05 21:40:06 +0100

    Added more puns

--- js/script.js
+++ js/script.js
@@ -9,7 +9,12 @@ $(document).ready(function() {
 		"Wir halten mehrere Records",
 		"Alles intakt",
 		"Im Angebot: Chai(kovsky)",
-		"Vinyl-st du etwas Neues hören?"
+		"Vinyl-st du etwas Neues hören?",
+		"Musik gehört indie Läden",
+		"Hier kommt alles punktlich",
+		"Nicht nur Metalle",
+		"Chillen mal anders",
+		"Singles in deiner Nähe"
 	];
 
 	var randomTagline = taglines[Math.floor(Math.random()*taglines.length)];
